3135b39b41e81131841c3ea001b121cf1f63e1bb
shayan <shayanjafarnejad75@gmail.com>
2025-09-26 17:16:36 +0330

    develop services logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Param} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, HttpCode, ConflictException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/model/User';
+import { Model } from 'mongoose';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/model/User';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UserService {
+    constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) { }
+
+    async Signup(createUserDto: CreateUserDto) {
+
+        const newUser = new this.UserModel(createUserDto)
+
+        newUser.save()
+
+        return { message: 'User Signuped successfully' }
+    }
 }
