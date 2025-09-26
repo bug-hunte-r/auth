6b0cd88a37ef8fd33f02f40982ada63239570ded
shayan <shayanjafarnejad75@gmail.com>
2025-09-26 17:02:32 +0330

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: 'src/.env'
+        }),
+        MongooseModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get('MONGO_URI'),
+            }),
+        })
+    ],
+
 })
 
 export class AppModule {}
